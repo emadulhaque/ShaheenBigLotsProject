7c5599e3cb4c85db82970e0b2237b1660d87f1a6
MD HAQUE <mdhaque@mds-mbp-3.lan>
2018-07-25 17:14:16 -0400

    TestDataShaheenBigLotsProject

---
+++ src/BigLotsTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BigLotsTest 
+{
+	WebDriver driver;
+	@Test
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/mdhaque/Downloads/chromedriver");
+		driver=new ChromeDriver();
+		driver.get("http://www.biglots.com/");
+		driver.navigate().refresh();
+		driver.manage().window().maximize();
+		
+	}
+}
